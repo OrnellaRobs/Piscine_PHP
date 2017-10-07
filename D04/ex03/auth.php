d193e3efba5d9f83216db49bce0207a4127086e5
OrnellaRobs <orazafin@student.42.fr>
2017-10-07 23:27:06 +0200

    d04_update

--- D04/ex03/auth.php
+++ D04/ex03/auth.php
@@ -1,3 +1,12 @@
 <?php
-function auth($login, $passwd);
+function auth($login, $passwd)
+{
+	$account = unserialize(file_get_contents('../private/passwd'));
+	foreach ($account as $elem)
+	{
+		if ($elem['login'] === $login && $elem['passwd'] === hash('whirlpool', $passwd))
+			return (TRUE);
+	}
+	return (FALSE);
+}
 ?>
